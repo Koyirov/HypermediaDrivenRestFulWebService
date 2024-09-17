3e76a2ca745b84b1f069865d531543bd99da0da0
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-17 11:38:49 +0200

    initial commit

---
+++ src/main/java/com/example/hypermediadrivenrestfulwebservice/HypermediaDrivenRestFulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hypermediadrivenrestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypermediaDrivenRestFulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypermediaDrivenRestFulWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hypermediadrivenrestfulwebservice/HypermediaDrivenRestFulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hypermediadrivenrestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypermediaDrivenRestFulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
